5b59a6d2b1d98064bd56f24cfbb99395e43efa03
0xGRAV3R <ycopyart.xyz@gmail.com>
2025-03-10 23:03:31 -0700

    🪙 Initial commit FE

---
+++ config/site.ts
@@ -0,0 +1,19 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Lend StableCoin",
+  mainNav: [
+    {
+      title: "Deposit/Withdraw",
+      href: "/",
+    },
+    {
+      title: "Liquidate",
+      href: "/liquidate",
+    },
+    {
+      title: "Config",
+      href: "/config",
+    },
+  ],
+};
